be4450d01388951500dab8e7c8c72d3b3e8e202e
davi <davijluna@gmail.com>
2024-01-04 00:00:49 -0300

    feat: adicionando script dos calculos no terminal

---
+++ script.js
@@ -0,0 +1,20 @@
+const soma = (number1, number2) => {
+  return number1 + number2
+}
+
+const sub = (number1, number2) => {
+  return number1 - number2
+}
+
+const mult = (number1, number2) => {
+  return number1 * number2
+}
+
+const div = (number1, number2) => {
+  return number1 / number2
+}
+
+console.log(soma(2, 5))
+console.log(sub(2, 5))
+console.log(mult(2, 5))
+console.log(div(2, 5))
