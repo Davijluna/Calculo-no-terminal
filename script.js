ad2782238dec7997e3c627fe6bda5b5c37c9c4f4
davi <davijluna@gmail.com>
2024-06-30 00:11:02 -0300

    feat: modificação temporaria

--- script.js
+++ script.js
@@ -1,36 +1,5 @@
-const soma = (number1, number2) => {
-  return number1 + number2
-}
-
-const sub = (number1, number2) => {
-  return number1 - number2
-}
-
-const mult = (number1, number2) => {
-  return number1 * number2
-}
-
-const div = (number1, number2) => {
-  return number1 / number2
-}
-
-const functionOperation = (firstName, Operation, lastName) => {
-  if(Operation === '+') return soma(firstName, lastName)
-  if(Operation === '-') return sub(firstName, lastName)
-  if(Operation === '*') return mult(firstName, lastName)
-  if(Operation === '/') return div(firstName, lastName)
-}
-
-console.log(functionOperation(1, '+', 1))
-
-const tagButton = document.getElementsByTagName('button')
-
-tagButton.addeventListerner('click', (target) => {
-  target.
-  console.log(target)
-})
 
 
 const teste = () => {
-  console.log("oi")
+ console.log("oi");
 }
